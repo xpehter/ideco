f29c93ebcd5d853d938afa355687844d4778fe62
Dmitriy <xpeh18@ya.ru>
2019-07-26 20:10:54 +0500

    Check common name

--- test_ssl/ssl_to_utm.sh
+++ test_ssl/ssl_to_utm.sh
@@ -18,6 +18,7 @@ UTM_IP='10.80.1.1'
 UTM_PASS="$(cat /root/.ssh/utm_pass)"
 UTM_USER='root'
 UTM_CERT='/var/opt/ideco/nginx_reverse_proxy/user_certs/help.ideco.ru.crt'
+UTM_REVERSE_PROXY_PID='$(cat /tmp/nginx_reverse_proxy.pid)' # PID nginx'а для reverse proxy указан в конфиге /var/opt/ideco/nginx_reverse_proxy/nginx.conf
 
 # Знаю про http://porkmail.org/era/unix/award.html#cat
 cat "${CERT_NEW_ACME_PATH}"help.ideco.ru.key > "${CERT_NEW}"
@@ -30,27 +31,34 @@ sshpass -p "${UTM_PASS}" scp -q -o StrictHostKeyChecking=no -o "UserKnownHostsFi
 
 # Проверив, что сертификат имеется, вытаскиваем из него дату окончания и приводим её к формату Unix Epoch, ибо так удобнее сравнивать
 if test -f "${CERT_NEW}"; then
-  CERT_NEW_END_DATE="$(date "+%s" --date="$(openssl x509 -in "${CERT_NEW}" -noout -enddate | cut -d= -f 2)")"
+  CERT_NEW_CN="$(openssl x509 -in "${CERT_NEW}" -noout -subject | cut -d= -f3)"
+  CERT_NEW_END_DATE_HUMAN="$(openssl x509 -in "${CERT_NEW}" -noout -enddate | cut -d= -f2)"
+  CERT_NEW_END_DATE="$(date "+%s" --date="${CERT_NEW_END_DATE_HUMAN}")"
 else
   echo "Missing ${CERT_NEW}" >&2
   exit 1
 fi
 if test -f "${CERT_OLD}"; then
-  CERT_OLD_END_DATE="$(date "+%s" --date="$(openssl x509 -in "${CERT_OLD}" -noout -enddate | cut -d= -f 2)")"
+  CERT_OLD_CN="$(openssl x509 -in "${CERT_OLD}" -noout -subject | cut -d= -f3)"
+  CERT_OLD_END_DATE_HUMAN="$(openssl x509 -in "${CERT_OLD}" -noout -enddate | cut -d= -f2)"
+  CERT_OLD_END_DATE="$(date "+%s" --date="${CERT_OLD_END_DATE_HUMAN}")"
 else
   echo "Missing ${CERT_OLD}" >&2
   exit 1
 fi
 
-if [ "${CERT_NEW_END_DATE}" -gt "${CERT_OLD_END_DATE}" ]; then
-  # В начале нужно менять в /var/opt/ideco/reverse_proxy_backend/storage.db , ибо именно там reverse_proxy_backend хранит сертификаты
-  
-  sshpass -p "${UTM_PASS}" scp -q -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" "${CERT_NEW}" "${UTM_USER}"@"${UTM_IP}":"${UTM_CERT}"
-  # PID nginx'а для reverse proxy указан в конфиге /var/opt/ideco/nginx_reverse_proxy/nginx.conf
-  sshpass -p "${UTM_PASS}" ssh -q -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" "${UTM_USER}"@"${UTM_IP}" 'kill -HUP $(cat /tmp/nginx_reverse_proxy.pid)'
-  echo "Certificate "$(openssl x509 -in "${CERT_OLD}" -noout -enddate | cut -d= -f 2)" has been replaced with certificate "$(openssl x509 -in "${CERT_NEW}" -noout -enddate | cut -d= -f 2)"" >&1
+if [ "${CERT_NEW_CN}" == "${CERT_OLD_CN}" ]; then
+  if [ "${CERT_NEW_END_DATE}" -gt "${CERT_OLD_END_DATE}" ]; then
+    # В начале нужно менять в /var/opt/ideco/reverse_proxy_backend/storage.db , ибо именно там reverse_proxy_backend хранит сертификаты
+    
+    sshpass -p "${UTM_PASS}" scp -q -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" "${CERT_NEW}" "${UTM_USER}"@"${UTM_IP}":"${UTM_CERT}"
+    sshpass -p "${UTM_PASS}" ssh -q -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" "${UTM_USER}"@"${UTM_IP}" "kill -HUP "${UTM_REVERSE_PROXY_PID}""
+    echo "Certificate "${CERT_NEW_CN}" replaced "${CERT_OLD_END_DATE_HUMAN}" -> "${CERT_NEW_END_DATE_HUMAN}"" >&1
+  else
+    echo "Certificate "${CERT_NEW_CN}" not replacement "${CERT_NEW_END_DATE_HUMAN}" < or = "${CERT_OLD_END_DATE_HUMAN}"" >&1
+  fi
 else
-  echo "Certificate "$(openssl x509 -in "${CERT_OLD}" -noout -enddate | cut -d= -f 2)" does not require replacement with certificate "$(openssl x509 -in "${CERT_NEW}" -noout -enddate | cut -d= -f 2)"" >&1
+  echo "Common name different "${CERT_NEW_CN}" != "${CERT_OLD_CN}"">&2
 fi
 # Убираем за собой
 rm -rf "${CERT_NEW}" "${CERT_OLD}"
